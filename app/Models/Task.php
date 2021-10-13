cbebd87ada6dd2b5d14269caf56868346a5b6bde
pshko <aram.khorsandi@gmail.com>
2021-10-13 20:28:29 +0330

    task and todo list relations

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -12,4 +13,9 @@ class Task extends Model
         'title',
         'todo_list_id'
     ];
+
+    public function todo_list() :BelongsTo
+    {
+        return $this->belongsTo(TodoList::class);
+    }
 }
